718df9ca5e02a08fce611b20b64e0bfeafae3040
Bhaveshpatil45 <bp700259@gmail.com>
2024-04-29 18:27:54 +0530

    Array

---
+++ Array/src/com/array/MarksSystem.java
@@ -0,0 +1,13 @@
+package com.array;
+
+import java.util.Scanner;
+
+public class MarksSystem {
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Pls enter marks of 5 subject : ");
+
+    }
+}
