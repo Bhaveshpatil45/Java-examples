04776266665fd59d1e4a54f5acc179271669ad4f
Bhaveshpatil45 <bp700259@gmail.com>
2024-07-17 13:32:52 +0530

    Core-Java

---
+++ Core-Java/src/com/bhavesh/practice/breakandcontinue/BreakDemo.java
@@ -0,0 +1,12 @@
+package com.bhavesh.practice.breakandcontinue;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            if (i == 4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
