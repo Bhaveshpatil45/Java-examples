44dfaffb661e622f62eb6e009cae7bb25dfba897
Bhaveshpatil45 <bp700259@gmail.com>
2024-10-16 19:55:47 +0530

    Hashmap

--- Core-Java/src/com/bhavesh/practice/collections/hashmapExamples/Exercise10_SpecifiedKey.java
+++ Core-Java/src/com/bhavesh/practice/collections/hashmapExamples/Exercise10_SpecifiedKey.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 
 public class Exercise10_SpecifiedKey {
     public static void main(String[] args) {
-        HashMap
+        HashMap<Integer,String> list_string = new HashMap<Integer, String>();
+        list_string.put(1,"Mumbai");
+        list_string.put(2,"Pune");
+        list_string.put(3,"Noida");
+        list_string.put(4,"Delhi");
+        list_string.put(5,"Indore");
+
+        String val = (String)list_string.get(3);
+        System.out.println("Value for key 3 is: " + val);
     }
 }
