ddb1491b659d4bdeebfb88826e7e53f5ed7927d9
Bhaveshpatil45 <bp700259@gmail.com>
2024-10-16 11:14:31 +0530

    Hashset

--- Core-Java/src/com/bhavesh/practice/collections/hashsetExamples/Exercise6_Clone.java
+++ Core-Java/src/com/bhavesh/practice/collections/hashsetExamples/Exercise6_Clone.java
@@ -1,5 +1,19 @@
 package com.bhavesh.practice.collections.hashsetExamples;
 
-public class Exercise6_Clone
-{
+import java.util.HashSet;
+
+public class Exercise6_Clone {
+    public static void main(String[] args) {
+        HashSet<String> list_string = new HashSet<String>();
+        list_string.add("Pune");
+        list_string.add("Mumbai");
+        list_string.add("Indore");
+        list_string.add("Kolkata");
+        list_string.add("Odisha");
+        System.out.println("Original array list: " + list_string);
+        HashSet<String> new_list = new HashSet<String>();
+        new_list = (HashSet)list_string.clone();
+        System.out.println("Cloned Hashset: " + new_list);
+
+    }
 }
