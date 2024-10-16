e7f9df3d6aa96a74e9ebcee99212de8e385baeb3
Bhaveshpatil45 <bp700259@gmail.com>
2024-10-16 20:14:25 +0530

    Hashmap

--- Core-Java/src/com/bhavesh/practice/collections/hashmapExamples/Exercise12_ValuesContained.java
+++ Core-Java/src/com/bhavesh/practice/collections/hashmapExamples/Exercise12_ValuesContained.java
@@ -1,4 +1,17 @@
 package com.bhavesh.practice.collections.hashmapExamples;
 
+import java.util.HashMap;
+
 public class Exercise12_ValuesContained {
+    public static void main(String[] args) {
+        HashMap<Integer,String> list_string = new HashMap<Integer, String>();
+        list_string.put(1,"Pune");
+        list_string.put(2,"Mumbai");
+        list_string.put(3,"Indore");
+        list_string.put(4,"Delhi");
+        list_string.put(5,"Noida");
+
+        System.out.println("Collection view is: " + list_string.values());
+    }
+
 }
