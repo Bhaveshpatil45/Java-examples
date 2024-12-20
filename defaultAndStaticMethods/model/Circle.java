a4ee05b4eab8d91a1049ac91a0775131656a0778
Bhaveshpatil45 <bp700259@gmail.com>
2024-12-20 19:39:56 +0530

    DefaultAndStaticMethod

---
+++ defaultAndStaticMethods/model/Circle.java
@@ -0,0 +1,15 @@
+package defaultAndStaticMethods.model;
+
+public class Circle  implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Circle drawn.");
+    }
+
+    @Override
+    public void paint() {
+        //Shape.super.paint();
+        System.out.println("Circle painted with base color.");
+    }
+}
---
+++ defaultAndStaticMethods/model/Square.java
@@ -0,0 +1,9 @@
+package defaultAndStaticMethods.model;
+
+public class Square implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Square drawn.");
+    }
+}
