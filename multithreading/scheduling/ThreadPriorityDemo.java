ca4f36f4a32f80b69e878a9996f4b3e74b9c9d3f
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-03 13:21:21 +0530

    multithreading

---
+++ multithreading/scheduling/ThreadPriorityDemo.java
@@ -0,0 +1,64 @@
+package multithreading.scheduling;
+
+// Concept in this demo : scheduling, priority
+
+class SupermanThread extends Thread{
+    int superCount = 1;
+
+    public void run(){
+        System.out.println("thread supermanThread started");
+        while (superCount <= 3){
+            System.out.println("Value: " + superCount + " in Thread supermanThread");
+            superCount++;
+        }
+        System.out.println("Thread SupermanThread completed");
+    }
+}
+
+class BatmanThread extends Thread{
+    int batCount = 1;
+
+    public void run(){
+        System.out.println("thread BatmanThread started");
+        while (batCount <= 3){
+            System.out.println("Value: " + batCount + " in Thread BatmanThread");
+            batCount++;
+        }
+        System.out.println("Thread BatmanThread completed");
+    }
+}
+
+class IronmanThread extends Thread{
+    int ironCount = 1;
+
+    public void run(){
+        System.out.println("thread IronmanThread started");
+        while (ironCount <= 3){
+            System.out.println("Value: " + ironCount + " in Thread IronmanThread");
+            ironCount++;
+        }
+        System.out.println("Thread IronmanThread completed");
+    }
+}
+
+public class ThreadPriorityDemo {
+    public static void main(String[] args) {
+        System.out.println("------------- Main thread starts here -------------");
+        SupermanThread supermanThread = new SupermanThread();
+        BatmanThread batmanThread = new BatmanThread();
+        IronmanThread ironmanThread = new IronmanThread();
+
+        Thread currentThread = Thread.currentThread();
+        System.out.println("Thread Name: " + currentThread.getName());
+
+        currentThread.setPriority(Thread.MAX_PRIORITY);
+        batmanThread.setPriority(Thread.MIN_PRIORITY);
+        ironmanThread.setPriority(Thread.NORM_PRIORITY);
+
+        supermanThread.start();
+        batmanThread.start();
+        ironmanThread.start();
+        System.out.println("------------- Main thread ends here -------------");
+
+    }
+}
