a56470d4e6ca7bb9219ccb7d34f3b6a04d9d9613
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-04 18:17:57 +0530

    multithreading

---
+++ multithreading/interThreading/InterThreadCommunicationDemo.java
@@ -0,0 +1,18 @@
+package multithreading.interThreading;
+
+public class InterThreadCommunicationDemo {
+    public static void main(String[] args) {
+        final CustomerAccount customerAccount = new CustomerAccount();
+        new Thread(){
+            public void run(){
+                customerAccount.withdraw(15000);
+            }
+        }.start();
+        new Thread(){
+            public void run(){
+                customerAccount.deposit(10000);
+            }
+        }.start();
+    }
+
+}
